7e43813f2543c5bfaae2a59bc09ead238a061a6c
DavidJi80 <jz_1106@163.com>
2018-10-10 13:23:08 +0800

    Spring Boot myBatis 声明式事务

--- mybatis/src/main/java/com/github/davidji80/springboot/mybatis/MybatisApplication.java
+++ mybatis/src/main/java/com/github/davidji80/springboot/mybatis/MybatisApplication.java
@@ -3,9 +3,11 @@ package com.github.davidji80.springboot.mybatis;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan("com.github.davidji80.springboot.mybatis.mapper")//将项目中对应的mapper类的路径
+@EnableTransactionManagement
 public class MybatisApplication {
 
     public static void main(String[] args) {
--- mybatis/src/main/java/com/github/davidji80/springboot/mybatis/service/impl/UserServiceImpl.java
+++ mybatis/src/main/java/com/github/davidji80/springboot/mybatis/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.github.davidji80.springboot.mybatis.model.User;
 import com.github.davidji80.springboot.mybatis.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service(value = "userService")
 public class UserServiceImpl implements UserService {
@@ -12,8 +13,11 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserMapper userMapper;
 
-    @Override
+    @Transactional
     public int addUser(User user) {
-        return userMapper.insertSelective(user);
+        int i=userMapper.insertSelective(user);
+        String a = null;
+        a.indexOf('c');//模拟抛出错误
+        return i;
     }
 }
