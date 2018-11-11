ec53cf2f7bb5a03c104e2edab7a7ca441b2d19cc
DavidJi80 <jz_1106@163.com>
2018-11-11 18:23:16 +0800

    Spring Boot：Boot2.0版本整合Neo4j

--- neo4j/src/main/java/com/github/davidji80/springboot/neo4j/Neo4jApplication.java
+++ neo4j/src/main/java/com/github/davidji80/springboot/neo4j/Neo4jApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 @SpringBootApplication
-@EnableNeo4jRepositories(basePackages = "com.github.davidji80.springboot.neo4j.dao")
+@EnableNeo4jRepositories
 public class Neo4jApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(Neo4jApplication.class, args);
     }
