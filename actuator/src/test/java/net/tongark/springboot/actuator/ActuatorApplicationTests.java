a50e2bfc904b6bf65e6c3a0c41d5e5538bfddcbb
David Ji <jz_1106@163.com>
2020-07-10 11:54:51 +0800

    Actuator

---
+++ actuator/src/test/java/net/tongark/springboot/actuator/ActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package net.tongark.springboot.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
