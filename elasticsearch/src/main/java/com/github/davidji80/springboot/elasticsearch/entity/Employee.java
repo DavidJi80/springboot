f7187b2777687e64c0b5b4e2a1cac124a3eea6b4
David Ji <jz_1106@163.com>
2020-07-27 18:30:44 +0800

    SQL : jOOQ - CRUD

--- elasticsearch/src/main/java/com/github/davidji80/springboot/elasticsearch/entity/Employee.java
+++ elasticsearch/src/main/java/com/github/davidji80/springboot/elasticsearch/entity/Employee.java
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 
-@Document(indexName = "company",type = "employee")
+@Document(indexName = "employee")
 public class Employee {
     @Id
     private String id;
